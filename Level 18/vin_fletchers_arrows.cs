e45fc3005621c2543a0905139cdb7f6d01a4267a
cbrown <8887437+carb0nxl@users.noreply.github.com>
2023-08-23 15:44:37 -0400

    - fixed access modifiers on (private backing) fields - added new properties using GET/SET - properties now include validation for shaft length

--- Level 18/vin_fletchers_arrows.cs
+++ Level 18/vin_fletchers_arrows.cs
@@ -1,37 +1,75 @@
-namespace DefaultNamespace;
 
         Console.WriteLine("Let's build an arrow!\n");
     
         Arrow arrow1 = new Arrow();
-
+        Arrow.Summary();
 
     public class Arrow
     {
-        // properties
-        public int _ShaftLength;
-        public Arrowhead _Arrowhead;
-        public Fletching _Fletching;
-        public double _TotalCost;
+        // FIELDS
+        private int _shaftLength;
+        private Arrowhead _arrowheadSelection;
+        private Fletching _fletchingSelection;
+        private double _totalCost;
         
-        // arrow material pricing
-        private const double STEEL_PRICE = 10.0;
-        private const double OBSIDIAN_PRICE = 5.0;
-        private const double WOOD_PRICE = 3.0;
+        private const int MIN_SHAFT_LENGTH = 60; // 60 cm
+        private const int MAX_SHAFT_LENGTH = 100; // 100 cm
+        
+        // ARROW MATERIAL PRICING (in gold)
+        private const double STEEL_PRICE = 10.00;
+        private const double OBSIDIAN_PRICE = 5.00;
+        private const double WOOD_PRICE = 3.00;
 
-        private const double PLASTIC_FLETCH_PRICE = 10.0;
-        private const double TURKEY_FEATHER_PRICE = 5.0;
-        private const double GOOSE_FEATHER_PRICE = 3.0;
+        private const double PLASTIC_FLETCH_PRICE = 10.00;
+        private const double TURKEYFEATHER_FLETCH_PRICE = 5.00;
+        private const double GOOSEFEATHER_FLETCH_PRICE = 3.00;
 
-        private const double SHAFT_PRICE_PER_CM = 0.05;
+        private const double SHAFT_PRICE_PER_CM = 0.05; // 0.05 gold per cm for shaft
+        
 
-        // constructor
-        public Arrow()
+        // CONSTRUCTOR
+        public Arrow(int shaftLength, Arrowhead arrowheadSelection, Fletching fletchingSelection, double totalCost)
         {
-            _Arrowhead = SelectArrowhead();
-            _Fletching = SelectFletching();
-            _ShaftLength = GetShaftLength();
-            _TotalCost = CalculateCost();
-            
+            ShaftLength = shaftLength;
+            ArrowheadSelection = arrowheadSelection;
+            FletchingSelection = fletchingSelection;
+            TotalCost = totalCost;
+        }
+        
+        // PROPERTIES
+        public Arrowhead ArrowheadSelection
+        {
+            get { return _arrowheadSelection; }
+            set { _arrowheadSelection = value; }
+        }
+        public Fletching FletchingSelection
+        {
+            get { return _fletchingSelection; }
+            set { _fletchingSelection = value; }
+        }
+        public int ShaftLength
+        {
+            get { return _shaftLength; }
+            set
+            {
+                if (value >= MIN_SHAFT_LENGTH && value <= MAX_SHAFT_LENGTH)
+                {
+                    _shaftLength = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Shaft length must be between 60 and 100 cm.");
+                }
+            }
+        }
+        public double TotalCost
+        {
+            get { return _totalCost; }
+            set { _totalCost = value; }
+        }
+
+        public void Summary()
+        {
             // Provide a summary of the user's arrow:
             Console.WriteLine("\nYour Custom Arrow:");
             Console.WriteLine($"Arrowhead: {_Arrowhead}");
@@ -42,23 +80,24 @@ namespace DefaultNamespace;
             Console.ReadLine();  // Wait for the user to press Enter before closing.
         }
 
-        
-        Arrowhead SelectArrowhead()
+        // METHOD: Arrow selection menu
+        Arrowhead SelectArrowheadMenu()
         {
             Console.WriteLine("Welcome to VIN FLETCHER'S ARROW SHOP");
             Console.WriteLine("====================================");
             Console.WriteLine("ARROWHEADS: Steel | Wood | Obsidian");
-            Console.WriteLine("A. Steel ----- 10 gold");
-            Console.WriteLine("B. Obsidian -- 5 gold");
-            Console.WriteLine("C. Wood ------ 3 gold");
+            Console.WriteLine($"A. Steel ----- {STEEL_PRICE} gold");
+            Console.WriteLine($"B. Obsidian -- {OBSIDIAN_PRICE} gold");
+            Console.WriteLine($"C. Wood ------ {WOOD_PRICE} gold");
 
             Console.Write("\nPlease input your choice of arrowhead using the LETTER only: ");
 
             // Run the ParseArrowheadInput method here to get input & assign it to (enum) arrowhead variable
             Arrowhead playerSelection = ParseArrowheadInput();
-
             return playerSelection;
         }
+        
+        // METHOD: Parsing user input for arrowhead selection
         Arrowhead ParseArrowheadInput()
         {
             while (true)
@@ -87,19 +126,22 @@ namespace DefaultNamespace;
             }
         }
         
-        Fletching SelectFletching()
+        // METHOD: Fletching selection menu
+        Fletching SelectFletchingMenu()
         {
             Console.WriteLine("====================================");
             Console.WriteLine("FLETCHING: Plastic | Turkey Feathers | Goose Feathers");
-            Console.WriteLine("A. Plastic -------------- 10 gold");
-            Console.WriteLine("B. Turkey Feathers ------ 5 gold");
-            Console.WriteLine("C. Goose Feathers ------- 3 gold");
+            Console.WriteLine($"A. Plastic -------------- {PLASTIC_FLETCH_PRICE} gold");
+            Console.WriteLine($"B. Turkey Feathers ------ {TURKEYFEATHER_FLETCH_PRICE} gold");
+            Console.WriteLine($"C. Goose Feathers ------- {GOOSEFEATHER_FLETCH_PRICE} gold");
 
             Console.Write("\nPlease input your choice of fletching using the LETTER only: ");
             
             Fletching playerSelection = ParseFletchingInput();
             return playerSelection;
         }
+        
+        //METHOD: Parsing user input for fletching
         Fletching ParseFletchingInput()
         {
             while (true)
@@ -166,7 +208,7 @@ namespace DefaultNamespace;
             switch (_Arrowhead)
             {
                 case Arrowhead.Steel:
-                    arrowheadCost = STEEL_PRICE;
+                    arrowheadCost = STEELPRICE;
                     break;
                 case Arrowhead.Obsidian:
                     arrowheadCost = OBSIDIAN_PRICE;
@@ -178,22 +220,22 @@ namespace DefaultNamespace;
                     arrowheadCost = 0;
                     break;
             }
+            
             switch (_Fletching)
             {
                 case Fletching.Plastic:
-                    fletchingCost = PLASTIC_FLETCH_PRICE;
+                    fletchingCost = PlasticFletchPrice;
                     break;
                 case Fletching.TurkeyFeather:
-                    fletchingCost = TURKEY_FEATHER_PRICE;
+                    fletchingCost = TurkeyFeatherPrice;
                     break;
                 case Fletching.GooseFeather:
-                    fletchingCost = GOOSE_FEATHER_PRICE;
+                    fletchingCost = GooseFeatherPrice;
                     break;
                 default:
                     fletchingCost = 0;
                     break;
             }
-            
             
             double shaftCost = SHAFT_PRICE_PER_CM * _ShaftLength;
 
